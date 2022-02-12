f9bcd38a32252e1ab3bd95a913332ce8d434af8c
Michal23 <michal.wegrzyn2137@gmail.com>
2022-02-12 22:38:07 +0100

    more logging

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ client.on('message', message => {
 
 function sendAndHandle(sender, msg) {
     sender.send(msg)
-        .catch(err => console.log("ERROR while sending msg: \"" + msg + "\". ERR: \"" + err + "\". Sender: " + sender.id));
+        .catch(err => {console.log("ERROR while sending msg: \"" + msg + "\". ERR: \"" + err + "\""); console.log(sender);});
 }
 
 client.login(process.env.TOKEN)
